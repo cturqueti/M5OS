c37dee9644e0165176e8d3dea580836eaaf00df0
Carlos Augusto D'Orazio Turqueti <cturqueti@yahoo.com.br>
2024-08-08 15:02:18 -0300

    Start2

---
+++ src/AppManager.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <WString.h>
+
+#include <map>
+#include <string>
+
+#include "App.h"
+
+class AppManager {
+   private:
+    std::map<std::string, App*> apps{};
+    AppManager();
+
+   public:
+    App* currentApp = nullptr;
+
+    static AppManager& getInstance();
+    void addApp(const std::string& name, App* app);
+    void openApp(const std::string& name);
+    void closeCurrentApp();
+    void tickCurrentApp() const;
+    void draw() const;
+    String getCurrentAppName() const;
+    std::map<std::string, App*> listApps();
+    App* getApp(const std::string& name);
+    App* getCurrentApp() const;  // Adicione esta linha
+};
